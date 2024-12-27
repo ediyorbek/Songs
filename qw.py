b678d6f69ceda1ebc13eaca1deb7e44a32a22b94
avocadok <avocadok@student.21-school.ru>
2024-12-27 12:21:33 +0500

    initial commit

---
+++ qw.py
@@ -0,0 +1,41 @@
+import os
+import yt_dlp
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+}
+
+itteration = 0
+max_itteration = 5
+
+driver = webdriver.Chrome()
+
+driver.get('https://uz.lyricsus.com/')
+
+links = [a.get_attribute('href') for a in driver.find_elements(By.XPATH, '//*[@id="post-19787"]/a')]
+while itteration < max_itteration:
+    for link in links:
+        if itteration >= max_itteration:
+            break
+
+        driver.get(link)
+        song_name = driver.find_element(By.XPATH, '//*[@id="main-area-header"]/h1').text
+        song_lyrics = driver.find_element(By.XPATH, '//div[@id="main-article"]').text
+
+        song_folder = f'parsed_songs/{song_name}'
+        if not os.path.exists(song_folder):
+            os.makedirs(song_folder)
+
+        text_file_path = os.path.join(song_folder, f'{song_name}.txt')
+        with open(text_file_path, 'w', encoding='utf-8') as text_file:
+            text_file.write(song_lyrics)
+
+        ydl_opts['outtmpl'] = os.path.join(song_folder, f'{song_name}.mp3')
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+
+        itteration += 1
+
+driver.quit()
